133ea9404cbadcb8f97445c4aebdf5a11931f971
yt <785661432@qq.com>
2019-06-10 16:55:48 +0800

    init3

--- app/src/main/java/com/example/yt/myapplication/ui/fragments/HostFragment.java
+++ app/src/main/java/com/example/yt/myapplication/ui/fragments/HostFragment.java
@@ -6,7 +6,9 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.example.yt.myapplication.R;
 
 public class HostFragment extends BaseFragment {
@@ -15,6 +17,7 @@ public class HostFragment extends BaseFragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        activity=getActivity();
         View view=FindView(inflater,container, R.layout.fragment_host);
         return view;
     }
