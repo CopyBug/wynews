32f548aa05537672d5e85573160ce3d00fb44c0c
Sion <785661432@qq.com>
2019-06-10 14:15:39 +0800

    commit10

--- app/src/main/java/com/example/yt/myapplication/SickMusic.java
+++ app/src/main/java/com/example/yt/myapplication/SickMusic.java
@@ -13,15 +13,33 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class SickMusic extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-  private   NavigationView navigationView ;
+    private CircleImageView imageView;
+    private TextView headerUsername;
+    private Button headerQiandao;
+    private TextView headerLeague;
+    private TextView headerMessage;
+    private TextView headerUser;
+    private TextView headerPifu;
+    private TextView headerVoice;
+
+
+    private   NavigationView navigationView ;
   private  Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sick_music);
+        initlayout();
+    }
+
+    private void initlayout() {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -31,6 +49,14 @@ public class SickMusic extends AppCompatActivity
         toggle.syncState();
         navigationView= (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        imageView = (CircleImageView) findViewById(R.id.imageView);
+        headerUsername = (TextView) findViewById(R.id.header_username);
+        headerQiandao = (Button) findViewById(R.id.header_qiandao);
+        headerLeague = (TextView) findViewById(R.id.header_league);
+        headerMessage = (TextView) findViewById(R.id.header_message);
+        headerUser = (TextView) findViewById(R.id.header_user);
+        headerPifu = (TextView) findViewById(R.id.header_pifu);
+        headerVoice = (TextView) findViewById(R.id.header_voice);
     }
 
 
