d6c15a170b6a3802c60e8ecd8b8aadcd1d8ff6ca
yt <785661432@qq.com>
2019-06-10 09:49:00 +0800

    init

---
+++ app/src/main/java/com/example/yt/myapplication/entitys/Userinfo.java
@@ -0,0 +1,22 @@
+package com.example.yt.myapplication.entitys;
+
+public class Userinfo {
+    private String username;
+    private String leave;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getLeave() {
+        return leave;
+    }
+
+    public void setLeave(String leave) {
+        this.leave = leave;
+    }
+}
