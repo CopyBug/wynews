918423d8bfadd084c870f81db0e447a0dd1fd122
pp <1096208568@qq.com>
2019-06-10 11:16:17 +0800

    yindaoye

--- app/src/main/java/com/example/yt/myapplication/GuideActivity.java
+++ app/src/main/java/com/example/yt/myapplication/GuideActivity.java
@@ -1,13 +1,40 @@
 package com.example.yt.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class GuideActivity extends AppCompatActivity {
+
+
+    private ImageView gudiepage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guide);
+        initView();
+        init();
+
+    }
+    private void init(){
+        Timer timer=new Timer();
+        TimerTask timerTask=new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(GuideActivity.this,SickMusic.class);
+                startActivity(intent);
+                GuideActivity.this.finish();
+            }
+        };
+        timer.schedule(timerTask,3000);
+    }
+
+    private void initView() {
+        gudiepage = (ImageView) findViewById(R.id.gudiepage);
     }
 }
