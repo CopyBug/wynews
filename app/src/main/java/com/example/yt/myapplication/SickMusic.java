80411f89c12063e841292aa135bb8d188c604d89
yt <785661432@qq.com>
2019-06-11 11:55:31 +0800

    ss

--- app/src/main/java/com/example/yt/myapplication/SickMusic.java
+++ app/src/main/java/com/example/yt/myapplication/SickMusic.java
@@ -11,7 +11,9 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.yt.myapplication.adapters.FragmentViewpageAdapter;
@@ -36,21 +38,24 @@ public class SickMusic extends AppCompatActivity
     private TextView headerPifu;
     private TextView headerVoice;
 
-    private List<String> title=new ArrayList<>();
-    private List<Fragment> fragments=new ArrayList<>();
-    private   NavigationView navigationView ;
-  private  Toolbar toolbar;
+    private List<String> title = new ArrayList<>();
+    private List<Fragment> fragments = new ArrayList<>();
+    private NavigationView navigationView;
+    private Toolbar toolbar;
+    private ImageView playStatus;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sick_music);
+        initView();
         initlayout();
     }
+
     private ViewPager contentVp;
 
 
     private TabLayout barTl;
-
 
 
     private void initlayout() {
@@ -61,7 +66,7 @@ public class SickMusic extends AppCompatActivity
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
-        navigationView= (NavigationView) findViewById(R.id.nav_view);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
         imageView = (CircleImageView) findViewById(R.id.imageView);
         headerUsername = (TextView) findViewById(R.id.header_username);
@@ -73,23 +78,55 @@ public class SickMusic extends AppCompatActivity
         headerVoice = (TextView) findViewById(R.id.header_voice);
         contentVp = (ViewPager) findViewById(R.id.content_vp);
         barTl = (TabLayout) findViewById(R.id.bar_tl);
-        AddFragment(new HostFragment(),"主页");
-        AddFragment(new MyselfFragment(),"我的");
-        AddFragment(new SearchFragment(),"发现");
-        AddFragment(new VideoFragment(),"视频");
-        FragmentViewpageAdapter fragmentViewpageAdapter=new FragmentViewpageAdapter(getSupportFragmentManager(),fragments,title);
+        AddFragment(new HostFragment(), "主页");
+        AddFragment(new MyselfFragment(), "我的");
+        AddFragment(new SearchFragment(), "发现");
+        AddFragment(new VideoFragment(), "视频");
+        FragmentViewpageAdapter fragmentViewpageAdapter = new FragmentViewpageAdapter(getSupportFragmentManager(), fragments, title);
         contentVp.setAdapter(fragmentViewpageAdapter);
         contentVp.setOffscreenPageLimit(5);
         barTl.setupWithViewPager(contentVp);
+        playStatus=findViewById(R.id.playStatus);
+        StartMusic();
+        playStatus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                boolean b = playStatus.getDrawable().getConstantState().equals(getResources().getDrawable(R.mipmap.icon_start).getConstantState());
+                if(b){
+                    playStatus.setImageResource(R.mipmap.icon_stop);
+                }else{
+                    playStatus.setImageResource(R.mipmap.icon_start);
+                }
+            }
+        });
+    }
+    public void StartMusic(){
+
     }
 
 
-public void AddFragment(Fragment fragment,String title){
+    public void AddFragment(Fragment fragment, String title) {
         fragments.add(fragment);
-    this.title.add(title);
-}
+        this.title.add(title);
+    }
+
+    @Override
+    public void setContentView(View view) {
+        super.setContentView(view);
+    }
+
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getTitle() + "") {
+            case "夜间模式":
+
+                break;
+        }
         return false;
+    }
+
+    private void initView() {
+        playStatus = (ImageView) findViewById(R.id.playStatus);
     }
 }
